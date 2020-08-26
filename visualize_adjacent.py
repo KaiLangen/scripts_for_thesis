4e88b180b11878a856bfcfa9e450e6dc06b723b3
Kai Langen (kjl408) <kai.langen@usask.ca>
2020-08-25 22:26:09 -0600

    Fix visualize_adjacent.py graph maker script

--- visualize_adjacent.py
+++ visualize_adjacent.py
@@ -1,62 +1,129 @@
-import re
-import numpy as np
+from glob import glob
+from matplotlib import pyplot as plt
+from matplotlib import gridspec
+from os.path import basename
+from numpy import array, stack, arange
 import sys
-import glob
-import matplotlib.pyplot as plt
+import matplotlib 
+
+fontsize = 18
+linewidth = 3
+markersize = 12
+font = {'weight' : 'bold',
+        'size'   : fontsize}
+
+matplotlib.rc('xtick', labelsize=fontsize)
+matplotlib.rc('ytick', labelsize=fontsize)
+matplotlib.rc('font', **font)
+
+def parse_data_from_filedump(file_dump):
+    file_data = {}
+    for line in file_dump:
+        line_data = line.strip().split(',')
+        gop = int(line_data[0])
+        tup = array([float(x) for x in line_data[1:]], dtype='float')
+        if sum(tup) == 0:
+            continue
+        if gop not in file_data:
+            file_data[gop] = []
+        file_data[gop].append(tup)
+    for gop in gops:
+        file_data[gop] = stack(file_data[gop], 1)
+    return file_data
 
 
-def parse_data():
-    pass
 
-def visualize_graph():
-    pass
+def get_video_from_filename(filename):
+    basefile = basename(file_name)
+    return '.'.join(basefile.split('.')[:-2])
 
-if __name__ == '__main__':       
-    if len(sys.argv) != 3:
-        print("Usage: python visualize_adjacent.py dir")
-        sys.exit()
+
+def construct_data_set():
+    data = {}
+    for video in videos:
+        data[video] = {}
+        for algo in colouring_algorithms:
+            file_name = "{}/{}.{}.dat".format(input_dir, video, algo)
+            with open(file_name, 'r') as f:
+                data[video][algo] = parse_data_from_filedump(f.readlines())
+    return data
+
+
+def visualize_data(video_name, gop, axis):
+    video_data = data[video_name]
+    data_subset = []
+    for algo in colouring_algorithms:
+        data_subset.append(video_data[algo][gop])
+    stacked = stack(data_subset, 2)
+    bitrate = stacked[1, :, :] / 1000
+    psnr = stacked[2, :, :]
+    orig_bitrate = stacked[3, :, 0] / 1000
+    orig_psnr = stacked[4, :, 0]
+
+    print("")
+    print("")
+    print(video_name, gop)
+    print("bitrate", orig_bitrate, bitrate)
+    print("psnr", orig_psnr, psnr)
+    axis.plot(
+        orig_bitrate,
+        orig_psnr,
+        label='Intra',
+        marker=markers[-2],
+        fillstyle='none',
+            linewidth=linewidth,
+        markersize=markersize)
+    axis.plot(bitrate[:, 0],
+            orig_psnr,
+            label='Theoretical Max',
+            marker=markers[-1],
+            linewidth=linewidth,
+            markersize=markersize)
+    axis.set_title('GOP={}'.format(gop))
+    for idx, algo in enumerate(colouring_algorithms):
+        axis.plot(
+            bitrate[:, idx],
+            psnr[:, idx],
+            label=labels[idx],
+            marker=markers[idx],
+            linestyle=line_style[idx],
+            linewidth=linewidth,
+            markersize=markersize)
+    axis.margins(x=0.15)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("usage python new_parse.py dir")
+        sys.exit(1)
     input_dir = sys.argv[1]
+    colouring_algorithms = ['hasan', 'discover', 'proposed']
+    labels = ['Hasan', 'MCI', 'MCR']
+    markers = ['X', 'D', 'v', 's', 'P']
+    line_style = ['--', '-.', ':']
+    files = {}
+    videos = []
+    gops = [2, 4, 8, 16]
+    for algo in colouring_algorithms:
+        files[algo] = glob('{}/*{}.dat'.format(input_dir, algo))
 
-    # read data and split
-    with open(discoverFile, 'r') as f:
-        discoverData = f.readlines()
-    discoverData = [line.strip().split(',') for line in discoverData]
-    discoverDict = {}
-    # reorganize data such that all data from the same GOP are binned together
-    for line in discoverData:
-        gopLevel = int(line[1])
-        tup = [float(x) for x in line[2:]]
-        if sum(tup) == 0:
-            continue
-        if gopLevel not in discoverDict:
-            discoverDict[gopLevel] = []
-        discoverDict[gopLevel].append(tup)
+    for file_name in files[algo]:
+        videos.append(get_video_from_filename(file_name))
 
-    # repeat the same process for proposed codec data
-    with open(proposedFile, 'r') as f:
-        proposedData = f.readlines()
-    proposedData = [line.strip().split(',') for line in proposedData]
-    proposedDict = {}
-    for line in proposedData:
-        gopLevel = int(line[1])
-        tup = [float(x) for x in line[2:]]
-        if sum(tup) == 0:
-            continue
-        if not gopLevel in proposedDict:
-            proposedDict[gopLevel] = []
-        proposedDict[gopLevel].append(tup)
+    data = construct_data_set()
 
-    for idx in range(2,5):
-        gop = 2**idx
-        plt.figure()
-        x0,y0 = zip(*discoverDict[1])
-        x1,y1 = zip(*discoverDict[idx])
-        x2,y2 = zip(*proposedDict[idx])
-        plt.plot(x0, y0, '-o', label="Discover: GOP=2", color='g')
-        plt.plot(x1, y1, '-o', label="Discover: GOP={}".format(gop), color='r')
-        plt.plot(x2, y2, '-o', label="Proposed: GOP={}".format(gop), color='b')
-        plt.title("Rate Distortion Comparison")
-        plt.xlabel("Average kB / frame")
-        plt.ylabel("Average PSNR")
-        plt.legend(loc='lower right')
-        plt.show()
+    for video in videos:
+        fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(21, 8), sharey=True)
+        for idx, gop in enumerate(gops[1:]):
+            axis = axes[idx]
+            visualize_data(video, gop, axis)
+            if idx == 0:
+                axis.set_ylabel("Average PSNR")
+            elif idx == 1:
+                axis.set_xlabel("Average mB / s")
+            elif idx == 2:
+                axis.legend(loc='lower right')
+        plt.tight_layout(pad=0.0, w_pad=0.0, h_pad=0.0)
+        plt.subplots_adjust(wspace=0.0)
+        plt.savefig('{}-rd-graph.png'.format(video))
+
