16bdeadf904b27105a7efc508bbb2ac8bf9e0633
Kai Langen (kjl408) <kai.langen@usask.ca>
2020-11-15 15:54:32 -0600

    Update SI / TI calculations to include Chroma spatial information

--- python_scripts/SI_TI_calculation.py
+++ python_scripts/SI_TI_calculation.py
@@ -31,6 +31,10 @@ def calculate_si(frame_data):
     si = np.hypot(sob_x, sob_y).std()
     return si
 
+def get_frame_from_array(arr, width, height):
+    frame_data = np.frombuffer(arr, dtype=np.uint8)
+    frame_data = (frame_data.astype(np.float64)).reshape(height, width)
+    return frame_data
 
 def calculate_ti(frame_data, previous_frame_data):
     """
@@ -49,23 +53,31 @@ def calculate_ti(frame_data, previous_frame_data):
 def si_ti_calculation(filename, width, height):
     frame_size = width * height
     si_values = []
+    chroma_si_u = []
+    chroma_si_v = []
     ti_values = [0.0]
-    previous_frame_data = None
+    previous_luma_data = None
 
     with open(filename, 'rb') as f:
         luma = f.read(frame_size)
-        chroma = f.read(frame_size // 2)
+        chroma_u = f.read(frame_size // 4)
+        chroma_v = f.read(frame_size // 4)
         while len(luma) > 0:
-            frame_data = np.frombuffer(luma, dtype=np.uint8)
-            frame_data = (frame_data.astype(np.float64)).reshape(height, width)
-            si_values.append(calculate_si(frame_data))
-            if previous_frame_data is not None:
-                ti_values.append(calculate_ti(frame_data, previous_frame_data))
+            luma_data = get_frame_from_array(luma, width, height)
+            chroma_u_data = get_frame_from_array(chroma_u, width // 2, height // 2)
+            chroma_v_data = get_frame_from_array(chroma_v, width // 2, height // 2)
+            si_values.append(calculate_si(luma_data))
+            # add calculations for Chroma spatial information 
+            chroma_si_u.append(calculate_si(chroma_u_data))
+            chroma_si_v.append(calculate_si(chroma_v_data))
+            if previous_luma_data is not None:
+                ti_values.append(calculate_ti(luma_data, previous_luma_data))
 
-            previous_frame_data = frame_data
+            previous_luma_data = luma_data
             luma = f.read(frame_size)
-            chroma = f.read(frame_size // 2)
-    return si_values, ti_values
+            chroma_u = f.read(frame_size // 4)
+            chroma_v = f.read(frame_size // 4)
+    return si_values, chroma_si_u, chroma_si_v, ti_values
 
 
 
@@ -75,11 +87,15 @@ if __name__ == '__main__':
     filename = sys.argv[1]
     width = 352
     height = 288
-    si_orig, ti_orig = si_ti_calculation(filename, width, height)
-    si = np.round(np.array(si_orig).astype(float), 3).tolist()
+    si_orig, chU_orig, chV_orig, ti_orig = si_ti_calculation(filename, width, height)
+    si_y = np.round(np.array(si_orig).astype(float), 3).tolist()
+    si_u = np.round(np.array(chU_orig).astype(float), 3).tolist()
+    si_v = np.round(np.array(chV_orig).astype(float), 3).tolist()
     ti = np.round(np.array(ti_orig).astype(float), 3).tolist()
 
     outfile = splitext(basename(filename))[0] + ".txt"
     with open(outfile, "w") as f:
-        f.write("max_si: {}\n".format(round(np.max(si), 3)))
+        f.write("max_si_Y: {}\n".format(round(np.max(si_y), 3)))
+        f.write("max_si_U: {}\n".format(round(np.max(si_u), 3)))
+        f.write("max_si_V: {}\n".format(round(np.max(si_v), 3)))
         f.write("max_ti: {}\n".format(round(np.max(ti), 3)))
